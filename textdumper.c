68ea5228287df34059f72e233dd1659ba4ae15a7
nia <nia@mailfence.com>
2025-08-29 22:24:27 +0200

    cutscene with samurai

--- textdumper.c
+++ textdumper.c
@@ -40,9 +40,10 @@ main(int argc, char **argv)
 	charset[0xa4] = "、";
 	charset[0xa8] = "ィ";
 	charset[0xeb] = "...";
-	charset[0x14] = "KNIGHT ";
-	charset[0x16] = "MERCENARY ";
-	charset[0x17] = "KNIGHT ";
+	charset[0x12] = "SAMURAI";
+	charset[0x14] = "KNIGHT";
+	charset[0x16] = "MERCENARY";
+	charset[0x17] = "KNIGHT";
 	charset[0x20] = " ";
 	charset[0x21] = "!";
 	charset[0x22] = "\"";
