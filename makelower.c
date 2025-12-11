f9f92d6d0a96ce8071bde9d7aff68bf39be649bf
nia <nia@mailfence.com>
2025-12-11 03:30:42 +0100

    makelower: fix some bugs

--- makelower.c
+++ makelower.c
@@ -11,13 +11,17 @@
  * Public domain.
  */
 
+#define ROM_SIZE	(8 << 16)
+#define DIALOGUE_START	(0x211e0)
+#define DIALOGUE_END	(0x2bf50)
+
 static const char *names[] = {
 	"I'M",
 	"I'VE",
 	"I'LL",
 	"NECROS",
-	"TANKIRIE CASTLE",
 	"TANKIRIE",
+	"TANKIRIE CASTLE",
 	"FIPONA FOREST",
 	"SIR",
 	"LORD",
@@ -40,8 +44,8 @@ static const char *names[] = {
 	"MK2",
 	"ONIROS",
 	"NIROS",
-	"MAJOROS",
 	"JOROS",
+	"MAJOROS",
 	"SEER",
 	"TAKOROS",
 	"HEBIROS",
@@ -54,6 +58,7 @@ static const char *names[] = {
 	"DAN",
 	"ANGELA",
 	"HOPE",
+	"NEW HOPE",
 	"MOUNT",
 	"XENO",
 	"HARRISON",
@@ -82,13 +87,27 @@ int
 main(int argc, char *argv[])
 {
 	FILE *origf, *newf;
-	uint8_t b1[256], b2[256];
+	uint8_t *b1, *b2;
 	size_t ret, offset;
+	size_t i, j;
+	bool upper = true;
 
 	if (argc < 3) {
 		fprintf(stderr,
 		    "%s: usage %s original.pce modified.pce\n",
 		    argv[0], argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	b1 = malloc(ROM_SIZE);
+	if (b1 == NULL) {
+		fprintf(stderr, "%s: %s\n", argv[0], strerror(errno));
+		return EXIT_FAILURE;
+	}
+
+	b2 = malloc(ROM_SIZE);
+	if (b2 == NULL) {
+		fprintf(stderr, "%s: %s\n", argv[0], strerror(errno));
 		return EXIT_FAILURE;
 	}
 
@@ -98,6 +117,77 @@ main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
+	offset = 0;
+	for (;;) {
+		ret = fread(b1 + offset, 1, 256, origf);
+		if (ret == 0) {
+			break;
+		}
+		offset += ret;
+	}
+
+	memcpy(b2, b1, ROM_SIZE);
+
+	for (i = DIALOGUE_START; i < DIALOGUE_END; ++i) {
+		if (!isascii(b1[i])) {
+			if (b1[i] == 0x0f || b1[i] == 0x04 ||
+			    b1[i] == 0x05 || b1[i] == 0xeb) {
+				upper = true;
+			}
+			continue;
+		}
+
+		if (b1[i] == '?' || b1[i] == '!' || b1[i] == '.') {
+			upper = true;
+			continue;
+		}
+
+		if (b1[i] >= 'A' && b1[i] <= 'Z') {
+			b2[i] = upper ? toupper(b1[i]) : tolower(b1[i]);
+			upper = false;
+		}
+	}
+
+	for (i = 0; i < (sizeof(names) / sizeof(char *)); ++i) {
+		uint8_t *tmp = b1 + DIALOGUE_START;
+		uint8_t *loc;
+
+		while ((loc = strcasestr(tmp, names[i])) != NULL) {
+			size_t off = loc - b1;
+			size_t max = off + strlen(names[i]);
+
+			if (off > DIALOGUE_END) {
+				/* outside dialogue range */
+				break;
+			}
+
+			/* check we're not in the middle of a word */
+			if ((off > 0 && isalpha(b2[off - 1])) ||
+			    (max < ROM_SIZE && isalpha(b2[max]))) {
+				goto skip;
+			}
+
+			b2[off] = toupper(b2[off]);
+			for (j = off; j < max; ++j) {
+				if (isspace(b2[j]) || b2[j] == 0x02) {
+					b2[j + 1] = toupper(b2[j + 1]);
+				}
+			}
+
+skip:
+			tmp = loc + 1;
+		}
+	}
+
+	/* find and fix "I"s */
+	for (i = DIALOGUE_START; i < DIALOGUE_END; ++i) {
+		if (b1[i] == 'I' && 
+		    (b1[i - 1] < 'A' || b1[i - 1] > 'Z') &&
+		    (b1[i + 1] < 'A' || b1[i + 1] > 'Z')) {
+			b2[i] = 'I';
+		}
+	}
+
 	newf = fopen(argv[2], "wb");
 	if (newf == NULL) {
 		fprintf(stderr, "%s: %s\n", argv[0], strerror(errno));
@@ -105,71 +195,14 @@ main(int argc, char *argv[])
 	}
 
 	offset = 0;
+	while (offset < ROM_SIZE) {
+		size_t n = 256;
 
-	for (;;) {
-		bool upper = true;
-		size_t i, j;
-
-		ret = fread(b1, 1, 256, origf);
-		if (ret == 0) {
-			break;
+		if (n > (ROM_SIZE - offset)) {
+			n = ROM_SIZE - offset;
 		}
 
-		for (i = 0; i < ret; ++i) {
-			b2[i] = b1[i];
-
-			if ((offset + i) < 0x211e0 ||
-			    (offset + i) > 0x2bf50) {
-				/* outside dialogue range */
-				continue;
-			}
-
-			if (!isascii(b1[i])) {
-				if (b1[i] == 0x0f || b1[i] == 0x04 ||
-				    b1[i] == 0x05 || b1[i] == 0xeb) {
-					upper = true;
-				}
-				continue;
-			}
-
-			if (b1[i] == '?' || b1[i] == '!' || b1[i] == '.') {
-				upper = true;
-				continue;
-			}
-
-			if (isalpha(b1[i])) {
-				b2[i] = upper ? toupper(b1[i]) : tolower(b1[i]);
-				upper = false;
-			}
-		}
-
-		for (i = 0; i < (sizeof(names) / sizeof(char *)); ++i) {
-			uint8_t *tmp = b1;
-			uint8_t *loc;
-
-			while ((loc = strstr(tmp, names[i])) != NULL) {
-				size_t off = loc - b1;
-				size_t max = off + strlen(names[i]);
-
-				/* check we're not in the middle of a word */
-				if ((off > 0 && isalpha(b2[off - 1])) ||
-				    isalpha(b2[max])) {
-					goto skip;
-				}
-
-				b2[off] = toupper(b2[off]);
-				for (j = off; j < max; ++j) {
-					if (isspace(b2[j]) || b2[j] == 0x02) {
-						b2[j + 1] = toupper(b2[j + 1]);
-					}
-				}
-
-skip:
-				tmp = loc + 1;
-			}
-		}
-
-		ret = fwrite(b2, 1, 256, newf);
+		ret = fwrite(b2 + offset, 1, n, newf);
 		if (ret == 0) {
 			break;
 		}
